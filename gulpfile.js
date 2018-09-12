7d9b9eb2489eff39a872d10a2df480e01471b9db
curti-s <matthewscurtis81@gmail.com>
2018-09-12 13:03:03 +0300

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,11 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+let gulp = require('gulp');
+let browserSync = require('browser-sync').create();
 
-gulp.task('serve', function() {
+gulp.task('serve', () => {
     browserSync.init({
-        server: {
-            baseDir: "./"
-        }
+        baseDir: "./"
     });
-    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('*.html').on('change',browserSync.reload);
 });
 
 gulp.task('default',['serve']);
